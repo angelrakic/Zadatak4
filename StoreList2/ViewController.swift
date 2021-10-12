0bdf50d22f13ae10d553a4c209a458bb35181f96
angelrakic <andjela.rakic@orioninc.com>
2021-10-12 09:53:14 +0200

    Initial Commit

---
+++ StoreList2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreList2
+//
+//  Created by Andjela Rakic on 10/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
