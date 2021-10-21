c1a0ba490c8491f77822833489a102959addf6b3
angelrakic <andjela.rakic@orioninc.com>
2021-10-21 13:00:03 +0200

    Nastavak zadatka

---
+++ StoreList2/ImageCacheManager.swift
@@ -0,0 +1,8 @@
+//
+//  ImageCacheManager.swift
+//  StoreList2
+//
+//  Created by Andjela Rakic on 10/15/21.
+//
+
+import Foundation
---
+++ StoreList2/StoreDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StoreDetailViewController.swift
+//  StoreList2
+//
+//  Created by Andjela Rakic on 10/15/21.
+//
+
+import UIKit
+
+class StoreDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
